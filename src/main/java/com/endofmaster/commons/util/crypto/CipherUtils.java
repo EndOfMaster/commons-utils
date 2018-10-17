ab7463e0b8f43fadc19695b4184a1afdf78e8cc2
EndOfMaster <843698826>
2018-10-17 15:22:49 +0800

    添加一个加密功能和包

--- src/main/java/com/endofmaster/commons/util/crypto/CipherUtils.java
+++ src/main/java/com/endofmaster/commons/util/crypto/CipherUtils.java
@@ -2,16 +2,23 @@ package com.endofmaster.commons.util.crypto;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+import java.security.spec.InvalidParameterSpecException;
 import java.util.zip.CRC32;
 
 /**
@@ -19,6 +26,10 @@ import java.util.zip.CRC32;
  * @update ZM.Wang
  */
 public class CipherUtils {
+
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     public static long crc32(String data) {
         if (data == null) {
@@ -71,6 +82,32 @@ public class CipherUtils {
             return cipher.doFinal(target);
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException
                 | IllegalBlockSizeException | InvalidKeyException e) {
+            throw new CryptoException(e);
+        }
+    }
+
+    public static byte[] decrypt(byte[] key, byte[] target, String algorithm, String transform, AlgorithmParameters parameters) throws CryptoException {
+        SecretKeySpec keySpec = new SecretKeySpec(key, algorithm);
+        return decrypt(keySpec, target, transform, parameters);
+    }
+
+    public static byte[] decrypt(Key key, byte[] target, String transform, AlgorithmParameters parameters) throws CryptoException {
+        try {
+            Cipher cipher = Cipher.getInstance(transform, "BC");
+            cipher.init(Cipher.DECRYPT_MODE, key, parameters);
+            return cipher.doFinal(target);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | BadPaddingException
+                | IllegalBlockSizeException | NoSuchProviderException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+            throw new CryptoException(e);
+        }
+    }
+
+    public static AlgorithmParameters generateIV(byte[] iv, String algorithm) throws CryptoException {
+        try {
+            AlgorithmParameters params = AlgorithmParameters.getInstance(algorithm);
+            params.init(new IvParameterSpec(iv));
+            return params;
+        } catch (NoSuchAlgorithmException | InvalidParameterSpecException e) {
             throw new CryptoException(e);
         }
     }
